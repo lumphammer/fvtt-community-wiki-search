ac93639bf7b3ca7a471939fa872b67cf278eeabc
Neil de Carteret <git@lumphammer.net>
2026-01-15 18:47:59 +0000

    move to workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-oxc";
 import tailwind from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), tailwind()],
+  plugins: [react(), tailwind(), cloudflare()],
 }));
